6e00bb6974f153db1d0c0e586ae32dac6ae2779c
bbachi <bhargavbachina@yahoo.com>
2020-08-13 09:53:50 -0500

    initial commit

---
+++ todo-app/components/Header.js
@@ -0,0 +1,13 @@
+  
+import React from 'react';
+
+const Header = () => {
+
+    return (
+        <div className="header">
+            <h1>Azure Static Web Apps With NextJS</h1>
+        </div>
+    )
+}
+
+export default Header
